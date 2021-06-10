6d484595e174a9ffd48f5db01e271e7dda4de45d
Dinmukhamed Tezekbayev <din.tezekbayev@gmail.com>
2021-06-11 00:31:30 +0600

    project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="robots" content="noindex">
     <meta name="googlebot" content="noindex">
-    <title>{{ isset($title) ? $title.' | Hammer CRM' : 'Hammer CRM' }}</title>
+    <title>Aibek Janzere</title>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
